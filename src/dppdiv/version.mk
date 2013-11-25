da8bfdcf07e14fb050bb9ac07b7e05a626f8610b
Trevor Cooper <tcooper@sdsc.edu>
2013-11-25 14:39:53 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/dppdiv/version.mk
+++ src/dppdiv/version.mk
@@ -1,3 +1,14 @@
-NAME    = dppdiv_$(ROLLCOMPILER)_$(ROLLMPI)_$(ROLLNETWORK)
-VERSION	= pll_1.0
-RELEASE	= 1
+NAME               = dppdiv_$(ROLLCOMPILER)_$(ROLLMPI)_$(ROLLNETWORK)
+VERSION            = pll_1.0
+RELEASE            = 1
+PKGROOT            = /opt/dppdiv
+
+SRC_SUBDIR         = dppdiv
+
+SOURCE_NAME        = dppdiv
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS        = $(SOURCE_PKG)
